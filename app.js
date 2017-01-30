c7743c03192314e96949b8813a24f03a97a60173
Rahul Reddy <mrahulreddy@users.noreply.github.com>
2017-01-30 12:47:28 +0530

    Add files via upload
    initial commit with:
    1- error handler code based on environment (specifically production and development)
    2- Hands on with req params (path variables) in express.

---
+++ app.js
@@ -0,0 +1,61 @@
+/*global module: true, angular:true, $:true, linkurl:true, require:true, console:true, __dirname:true */
+
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index); // the files start point is index.js, where '/' is the base path
+app.use('/users', users); // /users is the base path.
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+
+//development error handler
+// will print stacktrace
+if(app.get('env') === 'development'){
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+      res.render('error', {
+            message : err.message,
+            error : err
+    });
+  });
+}
+
+//production error handler
+// will not print stacktrace
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+    res.render('error',{
+        message : err.message,
+        error : err
+  });
+});
+
+module.exports = app;
